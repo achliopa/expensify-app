63dbe6ffc183b469f6bf69eb4eb09d5897844c28
achliopa <athchliopanos@gmail.com>
2017-11-24 07:29:54 +0200

    testing async actions lec.158

--- src/tests/actions/expenses.test.js
+++ src/tests/actions/expenses.test.js
@@ -1,11 +1,20 @@
 import configureMockStore from 'redux-mock-store';
 import thunk from 'redux-thunk';
-import { startAddExpense, addExpense, removeExpense, editExpense } from '../../actions/expenses';
+import { startAddExpense, addExpense, removeExpense, editExpense, setExpenses } from '../../actions/expenses';
 import expenses from '../fixtures/expenses';
 import database from '../../firebase/firebase';
 
 
 const createMockStore = configureMockStore([thunk]);
+
+beforeEach((done) => {
+	const expensesData = {};
+	expenses.forEach(({ id, description, note, amount, createdAt }) => {
+		expensesData[id] = { description, note, amount, createdAt };
+	});
+	database.ref('expenses').set(expensesData).then(() => done()).
+	catch((e) => console.log('error', e));
+});
 
 test('should setup remove expense action object', () => {
 	const action = removeExpense({ id: '123abc'});
@@ -104,3 +113,11 @@ test('should add expense with defaults to database and store', () => {
 // 		}	
 // 	});
 // });
+
+test('should setup set expense action object with data', () => {
+	const action = setExpenses(expenses);
+	expect(action).toEqual({
+		type: 'SET_EXPENSES',
+		expenses
+	});
+});
