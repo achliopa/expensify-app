6c0112577ce8a1f972d0c72e25606ea0cb958aa8
achliopa <athchliopanos@gmail.com>
2017-11-26 21:49:59 +0200

    added edit expense also on firebvase with tests

--- src/tests/actions/expenses.test.js
+++ src/tests/actions/expenses.test.js
@@ -1,6 +1,15 @@
 import configureMockStore from 'redux-mock-store';
 import thunk from 'redux-thunk';
-import { startRemoveExpense, startSetExpense, startAddExpense, addExpense, removeExpense, editExpense, setExpenses } from '../../actions/expenses';
+import { 
+			startEditExpense, 
+			startRemoveExpense, 
+			startSetExpense, 
+			startAddExpense, 
+			addExpense, 
+			removeExpense, 
+			editExpense, 
+			setExpenses 
+		} from '../../actions/expenses';
 import expenses from '../fixtures/expenses';
 import database from '../../firebase/firebase';
 
@@ -143,9 +152,28 @@ test('should remove expenses from firebase', (done) => {
 			type: 'REMOVE_EXPENSE',
 			id
 		});
-		return database.ref(`expenses/${id}`).once('value')
+		return database.ref(`expenses/${id}`).once('value');
 	}).then((snapshot) => {
 		expect(snapshot.val()).toBeFalsy();
+		done();
+	});
+});
+
+test('should edit expenses from firebase', (done) => {
+	const store = createMockStore({});
+	const id = expenses[1].id;
+	const updates = {note: 'Jest Test Note'};
+	store.dispatch(startEditExpense( id , updates))
+	.then(() => {
+		const actions = store.getActions();
+		expect(actions[0]).toEqual({
+			type: 'EDIT_EXPENSE',
+			id,
+			updates
+		});
+		return database.ref(`expenses/${id}`).once('value');
+	}).then((snapshot) => {
+		expect(snapshot.val().note).toEqual('Jest Test Note');
 		done();
 	});
 });
--- src/tests/components/EditExpensePage.test.js
+++ src/tests/components/EditExpensePage.test.js
@@ -3,15 +3,15 @@ import { EditExpensePage } from '../../components/EditExpensePage';
 import { shallow } from 'enzyme';
 import expenses from '../fixtures/expenses';
 
-let editExpense, startRemoveExpense, history, wrapper;
+let startEditExpense, startRemoveExpense, history, wrapper;
 
 beforeEach(() => {
-	editExpense = jest.fn();
+	startEditExpense = jest.fn();
 	startRemoveExpense = jest.fn();
 	history = { push: jest.fn() };
 	wrapper = shallow(
 		<EditExpensePage 
-			editExpense={editExpense} 
+			startEditExpense={startEditExpense} 
 			startRemoveExpense={startRemoveExpense} 
 			history={history}
 			expense={expenses[1]}
@@ -26,7 +26,7 @@ test('should render EditExpensePage correctly', () => {
 test('should handle editExpense', () => {
 	wrapper.find('ExpenseForm').prop('onSubmit')(expenses[1]);
 	expect(history.push).toHaveBeenCalledWith('/');
-	expect(editExpense).toHaveBeenCalledWith(expenses[1].id, expenses[1]);
+	expect(startEditExpense).toHaveBeenCalledWith(expenses[1].id, expenses[1]);
 });
 
 test('should handle removeExpense', () => {
