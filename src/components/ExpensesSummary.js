7290bb31966ba88abae738f77bc9be49d66bafec
Sakis Chliopanos <athchliopanos@gmail.com>
2017-11-28 14:06:48 +0000

    middle of section 17

--- src/components/ExpensesSummary.js
+++ src/components/ExpensesSummary.js
@@ -1,5 +1,6 @@
 import React from 'react';
 import { connect } from 'react-redux';
+import { Link } from 'react-router-dom';
 import selectExpenses from '../selectors/expenses';
 import selectExpensesTotal from '../selectors/expenses-total';
 import numeral from 'numeral';
@@ -32,14 +33,19 @@ import numeral from 'numeral';
 
 // COURSE CODE
 
-export const ExpensesSummary = (props) => {
-	const expenseWord = props.expenseCount === 1 ? 'expense' : 'expenses';
-	const formattedExpensesTotal = `${numeral(props.expensesTotal/100).format('0,0.00')}`;
+export const ExpensesSummary = ({ expenseCount, expensesTotal }) => {
+	const expenseWord = expenseCount === 1 ? 'expense' : 'expenses';
+	const formattedExpensesTotal = `${numeral(expensesTotal/100).format('0,0.00')}`;
 	return (
-		<div>
-			<h2> 
-				Viewing {props.expenseCount} {expenseWord} totalling {formattedExpensesTotal}&euro;
-			</h2>
+		<div className="page-header">
+			<div className="content-container">
+				<h1 className="page-header__title"> 
+					Viewing <span>{expenseCount}</span> {expenseWord} totalling <span>{formattedExpensesTotal}&euro;</span>
+				</h1>
+				<div className="page-header__actions">
+					<Link className="button" to="/create">Add Expense</Link>
+				</div>
+			</div>
 		</div>
 	);
 };
--- src/components/Header.js
+++ src/components/Header.js
@@ -6,10 +6,12 @@ import { startLogout } from '../actions/auth';
 export const Header = ({ startLogout }) => (
     <header className="header">
         <div className="content-container">
-            <Link className="header__title" to="/dashboard">
-                <h1>Expensify</h1>
-            </Link>
-            <button onClick={startLogout}>Logout</button>
+            <div className="header__content">
+                <Link className="header__title" to="/dashboard">
+                    <h1>Expensify</h1>
+                </Link>
+                <button className="button button--link" onClick={startLogout}>Logout</button>
+            </div>
         </div>
     </header>
 );
