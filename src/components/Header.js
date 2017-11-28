9ae61ac005b6da5933eb6105e9a49dfe0ae461fd
Sakis Chliopanos <athchliopanos@gmail.com>
2017-11-28 13:26:34 +0000

    going to test in heroku up to lec 173

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,14 +1,16 @@
 import React from 'react';
-import {NavLink} from 'react-router-dom';
+import {Link} from 'react-router-dom';
 import { connect } from 'react-redux';
 import { startLogout } from '../actions/auth';
 
 export const Header = ({ startLogout }) => (
-    <header>
-        <h1>Expensify</h1>
-        <NavLink activeClassName="is-active" to="/dashboard">Dashboard</NavLink>
-        <NavLink activeClassName="is-active" to="/create">Create Expense</NavLink>
-        <button onClick={startLogout}>Logout</button>
+    <header className="header">
+        <div className="content-container">
+            <Link className="header__title" to="/dashboard">
+                <h1>Expensify</h1>
+            </Link>
+            <button onClick={startLogout}>Logout</button>
+        </div>
     </header>
 );
 
